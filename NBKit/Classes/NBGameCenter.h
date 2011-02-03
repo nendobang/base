0f21b861c7bc72e268894fc6f40a5d9446711d6f
k-ya <fumihiro@gmail.com>
2011-02-03 21:18:29 +0900

    なんか間違ってたっぽいので二回目

--- NBKit/Classes/NBGameCenter.h
+++ NBKit/Classes/NBGameCenter.h
@@ -1,26 +1,49 @@
-
-
-
-
+#import <UIKit/UIKit.h>
+#import <GameKit/GameKit.h>
 
 /** 認証状態 */
 typedef enum {
-	NBGameCenterAuthStateNot,		///< 非認証状態
-	NBGameCenterAuthStateRequested,	///< 認証リクエスト
-	NBGameCenterAuthStateOK,		///< 認証完了
+	NBGameCenterAuthStateNot,				///< 非認証状態
+	NBGameCenterAuthStateRequested,			///< 認証リクエスト
+	NBGameCenterAuthStateOK,				///< 認証完了
 } NBGameCenterAuthState;
 
+//typedef enum {
+//	NBGameCenterAuthStateRetrieveFriedns,	///< フレンドリストを取得中
+//	NBGameCenterAuthStateComplete,			///< フレンドリストの取得完了
+//} NBGameCenter;
 
 // ゲームセンター
-@interface NBGameCenter : NSObject {
+@interface NBGameCenter : NSObject <GKLeaderboardViewControllerDelegate> {
 	GKLocalPlayer* localplayer_;
 	BOOL available_;
 	NBGameCenterAuthState authstate_;
-	NSArray* friends_;
+	NSArray* friends_;					///< フレンドのGKPlayer*の配列
+
+	NSString* leaderboardcategory_;
+	GKLeaderboardTimeScope leaderboardtimescope_;
+	UIViewController* leaderboardparent_;
 }
 @property (nonatomic, readonly) BOOL available;
 @property (nonatomic, readonly) NBGameCenterAuthState authstate;
++ (NBGameCenter*)instance;
++ (void)setup;
++ (void)cleanup;
+- (void)authenticate;
+- (void)retrieveFriends;
+- (void)showDefaultLeaderboard:(UIViewController*)parent;
+- (void)showDefaultLeaderboard:(UIViewController*)parent withCategory:(NSString*)category andTimeScope:(GKLeaderboardTimeScope)timescope;
+
+
+
+
+
 + (BOOL)checkGameCenterSupport;
+- (void)authenticationChanged;
+- (void)loadPlayerData:(NSArray*)ids;
+
+
+
 
 
 @end
--- NBKit/Classes/NBGameCenter.mm
+++ NBKit/Classes/NBGameCenter.mm
@@ -1,26 +1,61 @@
+#import "NBkit.h"
 #import "NBGameCenter.h"
 
 /**
  * ゲームセンター
  */
 @implementation NBGameCenter
+//
 @synthesize available=available_;
 @synthesize authstate=authstate_;
 
+static NBGameCenter* defaultGameCenter = nil;
+///
++ (NBGameCenter*)instance {
+	NB_ASSERT(defaultGameCenter != nil);
+	return defaultGameCenter;
+}
+///
++ (void)setup {
+	NB_ASSERT(defaultGameCenter == nil);
+	if (defaultGameCenter == nil) {
+		defaultGameCenter = [[NBGameCenter alloc] init];
+	}
+}
+///
++ (void)cleanup {
+	NB_ASSERT(defaultGameCenter != nil);
+	NB_RELEASE(defaultGameCenter);
+}
+
+
 /// コンストラクタ
 - (id)init {
+	NB_ASSERT(defaultGameCenter == nil);
 	if (self = [super init]) {
-		localplayer_ = nil;
+		defaultGameCenter = self;
 		authstate_ = NBGameCenterAuthStateNot;
 		available_ = [NBGameCenter checkGameCenterSupport];
 		if (available_) {
 			localplayer_ = [GKLocalPlayer localPlayer];
-			[self registerForAuthenticationNotification];
+			[[NSNotificationCenter defaultCenter]
+				addObserver:self
+				   selector:@selector(authenticationChanged)
+					   name:GKPlayerAuthenticationDidChangeNotificationName
+					 object:nil];
 		}
 	}
 	return self;
 }
-/// デバイスがゲームセンターをサポートしているか
+/// @internal デストラクタ
+- (void)dealloc {
+	[[NSNotificationCenter defaultCenter]
+		removeObserver:self
+				  name:GKPlayerAuthenticationDidChangeNotificationName
+				object:nil];
+	[super dealloc];
+}
+/// @internal デバイスがゲームセンターをサポートしているか
 + (BOOL)checkGameCenterSupport {
 	Class gcclass = NSClassFromString(@"GKLocalPlayer");
 	NSString* reqsysver = @"4.1";
@@ -28,38 +63,32 @@
 	BOOL osversupp = ([cursysver compare:reqsysver options:NSNumericSearch] != NSOrderedAscending);
 	return (gcclass && osversupp);
 }
-///
-- (void)authenticateLocalPlayer {
+/// ローカルプレイヤの認証を開始する
+- (void)authenticate {
 	if (authstate_ != NBGameCenterAuthStateNot) {
 		return;
 	}
+	authstate_ = NBGameCenterAuthStateRequested;
 	[localplayer_ authenticateWithCompletionHandler:^(NSError* err) {
 		if (err == nil) {
-			NS_LOG(@"SUCCEED");
-			NS_LOG(@"alias: %@", localplayer_.alias);
-			NS_LOG(@"playerID: %@", localplayer_.playerID);
+			NB_LOG(@"SUCCEED");
+			NB_LOG(@"alias: %@", localplayer_.alias);
+			NB_LOG(@"playerID: %@", localplayer_.playerID);
 		} else {
-			NS_LOG(@"FAILED");
+			NB_LOG(@"FAILED");
+			authstate_ = NBGameCenterAuthStateNot; // キャンセルされた（他に原因あるか？）
 		}
 	}];
 }
-///@internal
-- (void)registerForAuthenticationNotification {
-	[[NSNotificationCenter defaultCenter]
-		addObserver:self
-		   selector:@selector(authenticationChanged)
-			   name:GKPlayerAuthenticationDidChangeNotificationName
-			 object:nil];
-}
 ///
 - (void)authenticationChanged {
+	NB_RELEASE(friends_); // どっちにしろフレンド情報は破棄する
 	if ([GKLocalPlayer localPlayer].isAuthenticated) {
-		NS_LOG(@"AUTHENTICATED");
-		authstate_ = NBGameCenterAuthStateOK;
-		[self retrieveFriends];
+		NB_LOG(@"AUTHENTICATED");
+		authstate_ = NBGameCenterAuthStateOK; // 認証完了
 	} else {
-		NS_LOG(@"NOT AUTHENTICATED");
-		authstate_ = NBGameCenterAuthStateNot;
+		NB_LOG(@"NOT AUTHENTICATED");
+		authstate_ = NBGameCenterAuthStateRequested; // 一旦リクエスト状態に戻る
 	}
 }
 /// 友達リスト取得
@@ -68,30 +97,54 @@
 		[localplayer_ loadFriendsWithCompletionHandler:^(NSArray* friends, NSError* err) {
 			if (err == nil) {
 				[self loadPlayerData:friends];
-				NS_LOG(@"FRI: %d", friends.count);
-			
-			
-			
-			
+				NB_LOG(@"FRI: %d", friends.count);
 			} else {
-				NS_LOG(@"FAIL");
+				NB_LOG(@"FAIL");
 			}
 		}];
 	}
 }
-///
+/// @internal フレンドのIDからプレイヤデータを取得する
 - (void)loadPlayerData:(NSArray*)ids {
 	[GKPlayer loadPlayersForIdentifiers:ids withCompletionHandler:^(NSArray* players, NSError* err) {
 		if (err == nil) {
-			NS_LOG(@"PLAYERS: %d", [players retainCount]);
-			[friends_ release];
-			
-			
+			NB_LOG(@"PLAYERS: %d", [players retainCount]);
+			NB_RELEASE(friends_);
 			friends_ = [players retain];
 		} else {
 		}
 	}];
 }
+/// デフォルトのリーダーボードを表示する
+- (void)showDefaultLeaderboard:(UIViewController*)parent {
+	[self showDefaultLeaderboard:parent
+					withCategory:leaderboardcategory_
+					andTimeScope:leaderboardtimescope_];
+}
+/// カテゴリとタイムスコープを指定してデフォルトのリーダーボードを表示する
+- (void)showDefaultLeaderboard:(UIViewController*)parent withCategory:(NSString*)category andTimeScope:(GKLeaderboardTimeScope)timescope {
+	GKLeaderboardViewController* lvc =
+		[[GKLeaderboardViewController alloc] init];
+	if (lvc != nil) {
+		leaderboardparent_ = NB_RETAIN(parent);
+		lvc.category = category;
+		lvc.timeScope = timescope;
+		lvc.leaderboardDelegate = self;
+		[parent presentModalViewController:lvc animated:YES];
+	}
+}
+/// @internal リーダーボード表示終了時に呼ばれる
+- (void)leaderboardViewControllerDidFinish:(GKLeaderboardViewController*)lvc {
+	[leaderboardparent_ dismissModalViewControllerAnimated:YES];
+	// 最後にユーザが指定したであろう設定を保存しておく
+	NB_RELEASE(leaderboardcategory_);
+	leaderboardcategory_ = NB_RETAIN(lvc.category);
+	leaderboardtimescope_ = lvc.timeScope;
+	NB_RELEASE(leaderboardparent_);
+	NB_RELEASE(lvc);
+	NB_LOG(@"FINISHED");
+}
+
 
 
 
